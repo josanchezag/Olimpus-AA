ba1cc7c72593ad72c65c3ae4c1c97506b65edc45
Josimar Sanchez <josanchezag@gmail.com>
2025-05-19 15:53:22 -0500

    Initial project structure

---
+++ src/main/kotlin/com/Olimpus/Olimpus_AA/OlimpusAaApplication.kt
@@ -0,0 +1,11 @@
+package com.Olimpus.Olimpus_AA
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OlimpusAaApplication
+
+fun main(args: Array<String>) {
+	runApplication<OlimpusAaApplication>(*args)
+}
---
+++ src/test/kotlin/com/Olimpus/Olimpus_AA/OlimpusAaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Olimpus.Olimpus_AA
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OlimpusAaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
